83a13cc37fd80a0f38be92daea137b184e14842a
robert.lo <robert.lo@amtran.com.tw>
2024-03-08 23:47:29 +0800

    [refactor] - To add Summary column

--- Jira/jira_to_csv.py
+++ Jira/jira_to_csv.py
@@ -1,5 +1,5 @@
 import pandas
 
 df = pandas.read_csv("Amtran.csv")
-df_out = df[['Priority','Issue key','Status','Assignee','Created','Custom field (VendorCR)']]
+df_out = df[['Priority','Issue key','Status','Assignee','Created','Custom field (VendorCR)','Summary']]
 df_out.to_csv('Amtran_out.csv')
