8f87740b7afd035f375d0e9864a6f61595142cdd
robert.lo <robert.lo@amtran.com.tw>
2024-03-11 13:59:32 +0800

    [bug fix] - To remove index column for out CSV

--- Jira/jira_to_csv.py
+++ Jira/jira_to_csv.py
@@ -2,4 +2,4 @@ import pandas
 
 df = pandas.read_csv("Amtran.csv")
 df_out = df[['Priority','Issue key','Status','Assignee','Created','Custom field (VendorCR)','Summary']]
-df_out.to_csv('Amtran_out.csv')
+df_out.to_csv('Amtran_out.csv', index=False)
