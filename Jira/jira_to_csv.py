87cf3ac0c08547ce135bc85861deceeb1db39294
robert.lo <robert.lo@amtran.com.tw>
2024-03-15 10:31:01 +0800

    [import] - To add Gating stage field

--- Jira/jira_to_csv.py
+++ Jira/jira_to_csv.py
@@ -2,4 +2,5 @@ import pandas
 
 df = pandas.read_csv("Amtran.csv")
 df_out = df[['Priority','Issue key','Status','Assignee','Created','Custom field (VendorCR)','Summary']]
+# df_out = df[['Priority','Issue key','Status','Assignee','Created','Custom field (VendorCR)','Summary','Custom field (Gating Stage)']]
 df_out.to_csv('Amtran_out.csv', index=False)
