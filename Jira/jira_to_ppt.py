d210815c6622ae5355e65e324c3807e1b89bc026
robert.lo <robert.lo@amtran.com.tw>
2024-04-07 15:34:28 +0800

    [import] - To add jira to PPT functions

---
+++ Jira/jira_to_ppt.py
@@ -0,0 +1,103 @@
+# coding=UTF-8
+import sys
+import os
+import re
+import time
+import io
+import platform
+import pandas as pd
+
+from pptx import Presentation
+from pptx.util import Inches, Pt
+
+from jira import JIRA
+import json
+
+from datetime import datetime
+
+pwd = os.path.expanduser('~') + '/'
+
+
+FILE_PATH_JIRA_CSV_MAC= pwd + 'Documents/Github/Amt_work_platform/Jira/CSV/'
+FILE_PATH_JIRA_PIC_MAC= pwd + 'Documents/Github/Amt_work_platform/Jira/PIC/'
+FILE_PATH_JIRA_PPT_MAC= pwd + 'Documents/Github/Amt_work_platform/Jira/PPT/'
+
+FILE_PATH_JIRA_CSV=r"D:\work_platform\Github\Amt_work_platform\Jira\CSV\\"
+FILE_PATH_JIRA_PIC=r"D:\work_platform\Github\Amt_work_platform\Jira\PIC\\"
+FILE_PATH_JIRA_PPT=r"D:\work_platform\Github\Amt_work_platform\Jira\PPT\\"
+
+
+#----------------------------------------------------------------------
+def add_table(shapes_,number_):
+
+    item_ = ['OSD 操作介面','SYS 系統行為', 'Audio 爆音異音', 'PQ 畫質相關', 'Video 畫異、閃屏','待處理', 'Total' ]
+    cols = len(item_)
+    rows = 2
+    left = Inches(1.0)
+    top = Inches(2.0)
+    width = Inches(6.0)
+    height = Inches(0.8)
+
+    table = shapes_.add_table(rows, cols, left, top, width, height).table
+
+    # set column widths
+    # table.columns[0].width = Inches(2.0)
+    # table.columns[1].width = Inches(4.0)
+
+    for i in range(0,len(item_)):
+        table.columns[i].width = Inches(1.2)
+        table.cell(0, i).text = item_[i]
+
+    for i in range(0,len(item_)):
+        table.cell(1, i).text = str(number_[i])
+
+
+#----------------------------------------------------------------------
+def add_slide(prs, layout, title, number_,img_name_):
+    """Return slide newly added to `prs` using `layout` and having `title`."""
+    slide = prs.slides.add_slide(layout)
+    slide.shapes.title.text = title
+    shape = slide.shapes
+
+    add_table(shape,number_)
+
+    left = Inches(3)
+    top = Inches(4)
+    height = Inches(5)
+    
+    slide.shapes.add_picture(FILE_PATH_JIRA_PIC_MAC + img_name_, left, top, height)
+
+    return slide
+
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+
+    df = pd.read_csv(FILE_PATH_JIRA_CSV_MAC + 'Jira_Statistic_.csv')
+    # print(df)
+
+    prs = Presentation()
+    title_only_slide_layout = prs.slide_layouts[5]
+
+    prj_name = "GIGABYTE FO27Q2_2024"
+    list_ = df[df['Model'] == prj_name].values.flatten().tolist()[1:]
+    add_slide(prs, title_only_slide_layout, prj_name + time.strftime("%c"), list_, prj_name + '.png')
+
+    prj_name = "ASUS XG32WCMS_CSOT_2024"
+    list_ = df[df['Model'] == prj_name].values.flatten().tolist()[1:]
+    add_slide(prs, title_only_slide_layout, prj_name + time.strftime("%c"), list_, prj_name + '.png')
+
+    prj_name = "ASUS_XG32WCS-CSOT-2024"
+    list_ = df[df['Model'] == prj_name].values.flatten().tolist()[1:]
+    add_slide(prs, title_only_slide_layout, prj_name + time.strftime("%c"), list_, prj_name + '.png')
+
+    prj_name = "GIGABYTE M27UA_AUO_2024"
+    list_ = df[df['Model'] == prj_name].values.flatten().tolist()[1:]
+    add_slide(prs, title_only_slide_layout, prj_name + time.strftime("%c"), list_, prj_name + '.png')
+
+    prj_name = "GIGABYTE M27QA_BOE_2024"
+    list_ = df[df['Model'] == prj_name].values.flatten().tolist()[1:]
+    add_slide(prs, title_only_slide_layout, prj_name + time.strftime("%c"), list_, prj_name + '.png')
+    
+
+    prs.save(FILE_PATH_JIRA_PPT_MAC + 'jira_npi.pptx')
